b4a7c660894e3ef1f0c0031e91e502b34b4f7f38
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-05-29 13:46:43 +0900

    乱数を表示するボタンを作成した

--- SwiftUI-Chapter5/ContentView.swift
+++ SwiftUI-Chapter5/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            // ボタンを押すと実行されるところ
+            let num = Int.random(in: 0...100)
+            print(num)
+        }) {
+            // 見た目
+            Text("Random Button")
+        }
     }
 }
 
