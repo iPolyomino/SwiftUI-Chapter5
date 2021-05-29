ebae89b7225c28340777a41e13634a85b121ff78
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-05-29 13:30:00 +0900

    Initial Commit

---
+++ SwiftUI-Chapter5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Chapter5
+//
+//  Created by 萩倉丈 on 2021/05/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Chapter5/SwiftUI_Chapter5App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Chapter5App.swift
+//  SwiftUI-Chapter5
+//
+//  Created by 萩倉丈 on 2021/05/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Chapter5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
